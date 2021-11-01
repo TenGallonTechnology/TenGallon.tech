3e81688c0ff3e02eeda26105d4e9076b27fb0cb2
Kade-Powell <47295838+Kade-Powell@users.noreply.github.com>
2021-11-01 14:56:29 -0400

    adds meta tags

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-import logo from "../public/pbits.png";
+import logo from '../public/pbits.png';
 
 export const Header = () => {
   const router = useRouter();
@@ -16,15 +16,16 @@ export const Header = () => {
         <title>PBITS</title>
         <meta
           name="description"
-          content="A software company for small to medium businesses"
+          content="A software company based in Dublin GA specializing in web development and data analytics"
         />
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://www.pbits.online/" />
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <link rel="icon" href="/pbits.ico" />
       </Head>
 
       <Navbar bg="dark" variant="dark" expand="md">
-        <div style={{ width: "65px", marginLeft: "10px" }}>
+        <div style={{ width: '65px', marginLeft: '10px' }}>
           <Image src={logo} alt="PBITS" />
         </div>
 
@@ -38,9 +39,9 @@ export const Header = () => {
             <Link href="#services" passHref>
               <a
                 className={
-                  router.pathname.includes("services")
-                    ? "nav-item nav-link  text-primary"
-                    : "nav-item nav-link"
+                  router.pathname.includes('services')
+                    ? 'nav-item nav-link  text-primary'
+                    : 'nav-item nav-link'
                 }
               >
                 services
@@ -50,9 +51,9 @@ export const Header = () => {
             <Link href="#contact" passHref>
               <a
                 className={
-                  router.pathname.includes("contact")
-                    ? "nav-item nav-link  text-primary"
-                    : "nav-item nav-link"
+                  router.pathname.includes('contact')
+                    ? 'nav-item nav-link  text-primary'
+                    : 'nav-item nav-link'
                 }
               >
                 contact
@@ -62,9 +63,9 @@ export const Header = () => {
             <Link href="#about" passHref>
               <a
                 className={
-                  router.pathname.includes("#about")
-                    ? "nav-item nav-link  text-primary"
-                    : "nav-item nav-link"
+                  router.pathname.includes('#about')
+                    ? 'nav-item nav-link  text-primary'
+                    : 'nav-item nav-link'
                 }
               >
                 about
